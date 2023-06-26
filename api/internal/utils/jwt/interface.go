759b6cc7a2c7d0f30b98e1759c6fbdcae4026a0f
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-26 15:43:06 +0330

    feat: jwt authenticator

--- api/internal/utils/jwt/interface.go
+++ api/internal/utils/jwt/interface.go
@@ -4,3 +4,10 @@ type Authenticator interface {
 	GenerateToken(email string) (string, error)
 	ParseToken(token string) (string, error)
 }
+
+func New(cfg Config) Authenticator {
+	return &authenticator{
+		key:    cfg.PrivateKey,
+		expire: cfg.ExpireTime,
+	}
+}
