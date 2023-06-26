af3089afc569b2b37488b568781a6dfd67223d92
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-26 15:54:16 +0330

    init: auth

---
+++ api/internal/http/middleware/auth.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func (m *Middleware) Auth(ctx *fiber.Ctx) error {
+	if token := ctx.Get("x-token", ""); token != "" {
+		if email, err := m.JWTAuthenticator.ParseToken(token); err == nil {
+			ctx.Locals("email", email)
+
+			return ctx.Next()
+		} else {
+			log.Println(err)
+
+			return ctx.SendStatus(http.StatusUnauthorized)
+		}
+	} else {
+		return ctx.SendStatus(http.StatusBadRequest)
+	}
+}
