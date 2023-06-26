450d783a68a17287fd56ab9b04d7718d2bf71fdb
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-26 16:07:33 +0330

    add: api methods

--- api/internal/http/controller/user.go
+++ api/internal/http/controller/user.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/automated-pen-testing/api/internal/http/request"
@@ -12,7 +13,7 @@ func (c *Controller) Login(ctx *fiber.Ctx) error {
 	req := new(request.Login)
 
 	if err := ctx.BodyParser(req); err != nil {
-		log.Println(err)
+		log.Println(fmt.Errorf("[controller.Loing] failed to parse body error=%w", err))
 
 		return fiber.ErrBadRequest
 	}
@@ -23,13 +24,13 @@ func (c *Controller) Login(ctx *fiber.Ctx) error {
 
 	token, err := c.JWTAuthenticator.GenerateToken(req.Email)
 	if err != nil {
-		log.Println(err)
+		log.Println(fmt.Errorf("[controller.Loing] failed to create token error=%w", err))
 
 		return fiber.ErrInternalServerError
 	}
 
 	if er := c.RedisConnector.Set(req.Email, token, 0); er != nil {
-		log.Println(er)
+		log.Println(fmt.Errorf("[controller.Loing] failed to save token error=%w", er))
 
 		return fiber.ErrInternalServerError
 	}
--- api/main.go
+++ api/main.go
@@ -3,7 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/automated-pen-testing/api/internal/http/controller"
+	"github.com/automated-pen-testing/api/internal/http/middleware"
+	"github.com/automated-pen-testing/api/internal/storage/redis"
+	"github.com/automated-pen-testing/api/internal/utils/jwt"
 	"log"
+
+	"github.com/automated-pen-testing/api/internal/config"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -17,8 +23,35 @@ func main() {
 	// parse flags
 	flag.Parse()
 
+	// load configs
+	cfg := config.Load()
+
+	// create new jwt authenticator
+	authenticator := jwt.New(cfg.JWT)
+
+	// create redis connection
+	redisConnection, err := redis.New(cfg.Redis)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create middleware and controller
+	mid := middleware.Middleware{
+		JWTAuthenticator: authenticator,
+	}
+	ctl := controller.Controller{
+		JWTAuthenticator: authenticator,
+		RedisConnector:   redisConnection,
+	}
+
 	// creating a new fiber app
 	app := fiber.New()
+
+	app.Post("/login", ctl.Login)
+
+	auth := app.Use(mid.Auth)
+
+	auth.Get("/")
 
 	// starting app on choosing port
 	if err := app.Listen(fmt.Sprintf(":%d", *PortFlag)); err != nil {
