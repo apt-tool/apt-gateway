d7ce86271136d7434922118401f92de9e01e1350
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-18 09:46:53 +0330

    add: get for namespace

--- api/pkg/models/namespace/interface.go
+++ api/pkg/models/namespace/interface.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type Interface interface {
 	Create(namespace *Namespace) error
 	Delete(namespaceID uint) error
+	Get(namespaceIDs []uint, populate bool) ([]*Namespace, error)
 }
 
 type core struct {
@@ -17,4 +18,19 @@ func (c *core) Create(namespace *Namespace) error {
 
 func (c *core) Delete(namespaceID uint) error {
 	return c.db.Delete(&Namespace{}, "id = ?", namespaceID).Error
+}
+
+func (c *core) Get(namespaceIDs []uint, populate bool) ([]*Namespace, error) {
+	list := make([]*Namespace, 0)
+
+	query := c.db.Where("id in ?", namespaceIDs)
+	if populate {
+		query = query.Preload("Users").Preload("Projects")
+	}
+
+	if err := c.db.Where("id in ?", namespaceIDs).Find(&list).Error; err != nil {
+		return nil, err
+	}
+
+	return list, nil
 }
