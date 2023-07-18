3b88a925b6210bfe8eb3b1995b13f0eff631261b
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-18 12:26:45 +0330

    add: listern and serve

--- instructions/main.go
+++ instructions/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
 
+func main() {
+	port, _ := strconv.Atoi(os.Getenv("HTTP_PORT"))
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+		log.Fatalf("failed to start ftp server error=%v\n", err)
+	}
 }
