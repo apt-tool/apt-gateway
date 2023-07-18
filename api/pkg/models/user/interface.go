1e28b742dce6465a13bb74bcf5e9ef4d38897927
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-18 11:14:13 +0330

    update: user interface

--- api/pkg/models/user/interface.go
+++ api/pkg/models/user/interface.go
@@ -2,6 +2,7 @@ package user
 
 import "gorm.io/gorm"
 
+// Interface manages the user database methods.
 type Interface interface {
 	Create(user *User) error
 	Delete(userID uint) error
@@ -10,37 +11,47 @@ type Interface interface {
 	Validate(name, pass string) (*User, error)
 }
 
+func New(db *gorm.DB) Interface {
+	return &core{
+		db: db,
+	}
+}
+
 type core struct {
 	db *gorm.DB
 }
 
-func (c *core) Create(user *User) error {
+func (c core) Create(user *User) error {
 	return c.db.Create(user).Error
 }
 
-func (c *core) Delete(userID uint) error {
+func (c core) Delete(userID uint) error {
 	return c.db.Delete(&User{}, "id = ?", userID).Error
 }
 
-func (c *core) Update(userID uint, user *User) error {
+func (c core) Update(userID uint, user *User) error {
 	return c.db.Model(&User{}).Update("role", user.Role).Where("id = ?", userID).Error
 }
 
-func (c *core) Get() ([]*User, error) {
+func (c core) Get() ([]*User, error) {
 	list := make([]*User, 0)
 
 	if err := c.db.Find(&list).Error; err != nil {
-		return nil, err
+		return nil, ErrUserNotFound
 	}
 
 	return list, nil
 }
 
-func (c *core) Validate(name, pass string) (*User, error) {
+func (c core) Validate(name, pass string) (*User, error) {
 	user := new(User)
 
-	if err := c.db.First(&user).Where("username = ? and password = ?", name, pass).Error; err != nil {
-		return nil, err
+	if err := c.db.First(&user).Where("username = ?", name).Error; err != nil {
+		return nil, ErrUserNotFound
+	}
+
+	if user.Password != pass {
+		return nil, ErrIncorrectPassword
 	}
 
 	return user, nil
