24f0df6a690b45659c891793ab052690ad8c9276
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-18 12:39:13 +0330

    add: runner

--- api/internal/port/http/controller/controller.go
+++ api/internal/port/http/controller/controller.go
@@ -3,9 +3,11 @@ package controller
 import (
 	"github.com/automated-pen-testing/api/internal/storage/redis"
 	"github.com/automated-pen-testing/api/internal/utils/jwt"
+	"github.com/automated-pen-testing/api/pkg/models"
 )
 
 type Controller struct {
 	JWTAuthenticator jwt.Authenticator
+	Models           *models.Interface
 	RedisConnector   redis.Connector
 }
