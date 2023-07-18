c9b5cee57c3bd27880079ba6e4435b5bde40064d
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-18 11:10:52 +0330

    add: user interface methods

--- api/pkg/models/user/interface.go
+++ api/pkg/models/user/interface.go
@@ -1,9 +1,47 @@
 package user
 
+import "gorm.io/gorm"
+
 type Interface interface {
 	Create(user *User) error
 	Delete(userID uint) error
 	Update(userID uint, user *User) error
 	Get() ([]*User, error)
 	Validate(name, pass string) (*User, error)
+}
+
+type core struct {
+	db *gorm.DB
+}
+
+func (c *core) Create(user *User) error {
+	return c.db.Create(user).Error
+}
+
+func (c *core) Delete(userID uint) error {
+	return c.db.Delete(&User{}, "id = ?", userID).Error
+}
+
+func (c *core) Update(userID uint, user *User) error {
+	return c.db.Model(&User{}).Update("role", user.Role).Where("id = ?", userID).Error
+}
+
+func (c *core) Get() ([]*User, error) {
+	list := make([]*User, 0)
+
+	if err := c.db.Find(&list).Error; err != nil {
+		return nil, err
+	}
+
+	return list, nil
+}
+
+func (c *core) Validate(name, pass string) (*User, error) {
+	user := new(User)
+
+	if err := c.db.First(&user).Where("username = ? and password = ?", name, pass).Error; err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
--- api/pkg/models/user/user.go
+++ api/pkg/models/user/user.go
@@ -1,10 +1,14 @@
 package user
 
-import "github.com/automated-pen-testing/api/pkg/models"
+import (
+	"github.com/automated-pen-testing/api/pkg/enum"
+	"github.com/automated-pen-testing/api/pkg/models"
+)
 
 // User is the base entity of our clients
 type User struct {
 	models.BaseModel
 	Username string
 	Password string
+	Role     enum.Role
 }
