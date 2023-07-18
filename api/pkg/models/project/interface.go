c9d7a1042c573c33b30f5198f942c083adc25339
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-18 09:50:15 +0330

    add: project interface methods

--- api/pkg/models/project/interface.go
+++ api/pkg/models/project/interface.go
@@ -1,7 +1,35 @@
 package project
 
+import "gorm.io/gorm"
+
 type Interface interface {
 	Create(project *Project) error
 	Delete(projectID uint) error
-	GetByID(projectID uint) error
+	GetByID(projectID uint) (*Project, error)
+}
+
+type core struct {
+	db *gorm.DB
+}
+
+func (c *core) Create(project *Project) error {
+	return c.db.Create(project).Error
+}
+
+func (c *core) Delete(projectID uint) error {
+	return c.db.Delete(&Project{}, "id = ?", projectID).Error
+}
+
+func (c *core) GetByID(projectID uint) (*Project, error) {
+	prj := new(Project)
+
+	if err := c.db.First(&prj, "id = ?", projectID).Error; err != nil {
+		return nil, err
+	}
+
+	if prj.ID != projectID {
+		return nil, nil
+	}
+
+	return prj, nil
 }
