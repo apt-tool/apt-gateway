8c1e285a0b75669860ab7357962e28f91a750a02
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-19 12:06:22 +0330

    add: process handler

--- api/internal/core/handler/handler.go
+++ api/internal/core/handler/handler.go
@@ -1,11 +1,24 @@
 package handler
 
 import (
+	"github.com/automated-pen-testing/api/internal/core/request"
 	"github.com/automated-pen-testing/api/pkg/client"
 	"github.com/automated-pen-testing/api/pkg/models"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 type Handler struct {
 	Client *client.Client
 	Models *models.Interface
+}
+
+func (h Handler) Process(ctx *fiber.Ctx) error {
+	req := new(request.Request)
+
+	if err := ctx.BodyParser(&req); err != nil {
+		return err
+	}
+
+	return ctx.SendStatus(fiber.StatusOK)
 }
