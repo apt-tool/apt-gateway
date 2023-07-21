ce6a13304d5a12c52187769fa1ee8da8afb09446
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-21 11:37:22 +0330

    add: worker

--- api/internal/core/worker/worker.go
+++ api/internal/core/worker/worker.go
@@ -1,1 +1,25 @@
 package worker
+
+import "log"
+
+// worker is the smallest unit of our core
+type worker struct {
+	channel chan int
+}
+
+// work method will do the logic of penetration testing
+func (w worker) work() {
+	for {
+		id := <-w.channel
+
+		// todo: remove history (if exists)
+		// todo: analyse
+		// todo: use model
+		// todo: get instructions
+		// todo: execute instructions
+		// todo: save into log file
+		// todo: update database
+
+		log.Println(id)
+	}
+}
