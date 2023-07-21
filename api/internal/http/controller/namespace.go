8bc56a21362870c1f2b25743c0222c3f6898ff71
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-21 09:43:07 +0330

    add: create namespace

--- api/internal/http/controller/namespace.go
+++ api/internal/http/controller/namespace.go
@@ -1,9 +1,28 @@
 package controller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/automated-pen-testing/api/internal/http/request"
+	"github.com/automated-pen-testing/api/pkg/models/namespace"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func (c Controller) CreateNamespace(ctx *fiber.Ctx) error {
+	req := new(request.NamespaceRequest)
 
+	if err := ctx.BodyParser(&req); err != nil {
+		return err
+	}
+
+	tmp := namespace.Namespace{
+		Name: req.Name,
+	}
+
+	if err := c.Models.Namespaces.Create(&tmp); err != nil {
+		return err
+	}
+
+	return ctx.SendStatus(fiber.StatusOK)
 }
 
 func (c Controller) DeleteNamespace(ctx *fiber.Ctx) error {
