301154ce14c4766c688c5d3ad26a23f2395239e5
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-21 11:29:40 +0330

    add: secure middleware

--- api/internal/config/core/config.go
+++ api/internal/config/core/config.go
@@ -1,8 +1,9 @@
 package core
 
 type Config struct {
-	Timeout    int  `koanf:"timeout"`
-	Port       int  `koanf:"port"`
-	Enable     bool `koanf:"enable"`
-	Preemptive bool `koanf:"preemptive"`
+	Timeout    int    `koanf:"timeout"`
+	Port       int    `koanf:"port"`
+	Enable     bool   `koanf:"enable"`
+	Preemptive bool   `koanf:"preemptive"`
+	Secret     string `koanf:"secret"`
 }
--- api/internal/core/handler/handler.go
+++ api/internal/core/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"strconv"
 
+	"github.com/automated-pen-testing/api/internal/utils/crypto"
 	"github.com/automated-pen-testing/api/pkg/client"
 	"github.com/automated-pen-testing/api/pkg/models"
 
@@ -12,6 +13,17 @@ import (
 type Handler struct {
 	Client *client.Client
 	Models *models.Interface
+	Secret string
+}
+
+func (h Handler) Secure(ctx *fiber.Ctx) error {
+	cypher := ctx.Get("x-secure", "")
+
+	if crypto.GetMD5Hash(cypher) != h.Secret {
+		return ctx.Status(fiber.StatusForbidden).SendString("cannot access core")
+	}
+
+	return ctx.Next()
 }
 
 func (h Handler) Process(ctx *fiber.Ctx) error {
@@ -21,7 +33,7 @@ func (h Handler) Process(ctx *fiber.Ctx) error {
 }
 
 func (h Handler) Register(app *fiber.App) {
-	app.Get("/:project_id", h.Process)
+	app.Get("/:project_id", h.Secure, h.Process)
 	app.Get("/health", func(ctx *fiber.Ctx) error {
 		return ctx.SendStatus(fiber.StatusOK)
 	})
