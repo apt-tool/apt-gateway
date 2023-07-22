63827ed6d9fcb7c8118c53b55d135f47f811727f
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-22 19:44:36 +0330

    update: ftp secure

--- instructions/main.go
+++ instructions/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,14 +16,32 @@ const (
 	mainDir = "attacks"
 )
 
+func getMD5Hash(text string) string {
+	hash := md5.Sum([]byte(text))
+
+	return hex.EncodeToString(hash[:])
+}
+
 func downloadHandler(w http.ResponseWriter, r *http.Request) {
+	secret := os.Getenv("FTP_SECRET")
+
 	path := r.URL.Query().Get("path")
+
+	cypher := getMD5Hash(fmt.Sprintf("%s%s", secret, path))
+
+	if r.Header.Get("x-token") != cypher {
+		w.WriteHeader(http.StatusForbidden)
+
+		return
+	}
 
 	data, err := os.ReadFile(fmt.Sprintf("%s/%s", mainDir, path))
 	if err != nil {
 		log.Printf("failed to find file address=%s\n", path)
 
 		_, _ = fmt.Fprint(w, err)
+
+		return
 	}
 
 	http.ServeContent(w, r, path, time.Now(), bytes.NewReader(data))
