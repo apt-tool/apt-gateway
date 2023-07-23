621e80157fc8ce93176dd972de1daa39f832ecb3
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-23 09:05:15 +0330

    add: access middleware

--- instructions/internal/http/handler.go
+++ instructions/internal/http/handler.go
@@ -7,21 +7,21 @@ type Handler struct {
 }
 
 func (h Handler) Health(ctx *fiber.Ctx) error {
-
+	return nil
 }
 
 func (h Handler) Download(ctx *fiber.Ctx) error {
-
+	return nil
 }
 
 func (h Handler) Upload(ctx *fiber.Ctx) error {
-
+	return nil
 }
 
 func (h Handler) List(ctx *fiber.Ctx) error {
-
+	return nil
 }
 
 func (h Handler) Execute(ctx *fiber.Ctx) error {
-
+	return nil
 }
--- instructions/internal/http/middleware.go
+++ instructions/internal/http/middleware.go
@@ -1,1 +1,20 @@
 package http
+
+import (
+	"fmt"
+
+	"github.com/automated-pen-testing/instructions/internal/crypto"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func (h Handler) AccessMiddleware(ctx *fiber.Ctx) error {
+	path := ctx.Query("path", "")
+	cypher := crypto.GetMD5Hash(fmt.Sprintf("%s%s", h.AccessKey, path))
+
+	if cypher != ctx.Get("x-token") {
+		return fiber.ErrForbidden
+	}
+
+	return ctx.Next()
+}
