ac443e8c415aae61b0c191be8bc9366a8ecc2a76
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-23 09:14:21 +0330

    add: ftp main

--- instructions/main.go
+++ instructions/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
 
+	"github.com/automated-pen-testing/instructions/internal/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	// get env variables
+	port, _ := strconv.Atoi(os.Getenv("HTTP_PORT"))
+	private := os.Getenv("PRIVATE_KEY")
+	access := os.Getenv("ACCESS_KEY")
+
+	// create new fiber app
+	app := fiber.New()
+
+	// create new handler
+	h := http.Handler{
+		AccessKey:  access,
+		PrivateKey: private,
+	}
+
+	app.Get("/health", h.Health)
+	app.Get("/download", h.AccessMiddleware, h.Download)
+
+	if err := app.Listen(fmt.Sprintf(":%d", port)); err != nil {
+		log.Fatal(fmt.Errorf("failed to start ftp server error=%w", err))
+	}
 }
