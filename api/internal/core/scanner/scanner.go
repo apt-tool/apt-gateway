f395d9a00cf790ddc7a809d78f793c4e6d28d8cf
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-08 10:10:47 +0330

    update: scanner command

--- api/internal/core/scanner/scanner.go
+++ api/internal/core/scanner/scanner.go
@@ -1,6 +1,24 @@
 package scanner
 
+import (
+	"fmt"
+	"os/exec"
+)
+
+var (
+	template = "./scanner --url=%s --blockRegex true --reportFormat json --reportName %s --skipWAFIdentification true --skipWAFBlockCheck true --noEmailReport true"
+)
+
 // Scan a host by using apt-scanner
-func Scan(host string) (Report, error) {
+func Scan(host, name string) (Report, error) {
+	// create command
+	command := fmt.Sprintf(template, host, name)
+
+	// execute command
+	cmd := exec.Command(command)
+	if err := cmd.Start(); err != nil {
+		return Report{}, err
+	}
+
 	return Report{}, nil
 }
