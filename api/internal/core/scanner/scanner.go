c626947fdd29328575edf9ea8bd4fd5af26244d3
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-08 09:57:52 +0330

    init: scanner

---
+++ api/internal/core/scanner/scanner.go
@@ -0,0 +1,6 @@
+package scanner
+
+// Scan a host by using apt-scanner
+func Scan(host string) (Report, error) {
+	return Report{}, nil
+}
---
+++ api/internal/core/scanner/type.go
@@ -0,0 +1,7 @@
+package scanner
+
+// Report is our type in order to convert scanner
+// report to our system report
+type Report struct {
+	Vulnerabilities []string
+}
