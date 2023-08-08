f5e56c866bfb5e5630175c09bbc1bdbbfe3ed2a3
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-08 10:16:11 +0330

    update: scan process

--- api/internal/core/scanner/scanner.go
+++ api/internal/core/scanner/scanner.go
@@ -17,24 +17,26 @@ var (
 
 // Scan a host by using apt-scanner
 func Scan(host, name string) ([]string, error) {
+	r := new(report)
+
 	// create command
 	command := fmt.Sprintf(template, host, name)
 
 	// execute command
 	cmd := exec.Command(command)
 	if err := cmd.Start(); err != nil {
-		return report{}.vulnerabilities, err
+		return r.vulnerabilities, err
 	}
 
 	// read report file
 	context, err := os.ReadFile(reportsDir + name + ".json")
 	if err != nil {
-		return report{}.vulnerabilities, err
+		return r.vulnerabilities, err
 	}
 
-	r := new(report)
+	// convert our json object into our report
 	if er := json.Unmarshal(context, r); er != nil {
-		return report{}.vulnerabilities, err
+		return r.vulnerabilities, err
 	}
 
 	return r.vulnerabilities, nil
