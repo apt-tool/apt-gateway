899d904f45fac13b3e5060220f7ed5bbed65b50f
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-06 20:07:30 +0330

    update: erorr handler

--- internal/http/controller/handler/error.go
+++ internal/http/controller/handler/error.go
@@ -10,66 +10,101 @@ type ErrorHandler struct {
 	DevMode bool
 }
 
-func (e ErrorHandler) ErrBodyParser(ctx *fiber.Ctx, err error) error {
+func (e ErrorHandler) Error(ctx *fiber.Ctx, err error, message string) error {
+	if e.DevMode {
+		log.Printf("[err.Error] error: %v\n", err)
+	}
+
+	return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+		"error":   err,
+		"message": message,
+	})
+}
+
+func (e ErrorHandler) ErrBodyParser(ctx *fiber.Ctx, err error, message string) error {
 	if e.DevMode {
 		log.Printf("[err.BodyParser] error: %v\n", err)
 	}
 
-	return ctx.Status(fiber.StatusBadRequest).SendString(err.Error())
+	return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+		"error":   err,
+		"message": message,
+	})
 }
 
-func (e ErrorHandler) ErrQueryParser(ctx *fiber.Ctx, err error) error {
+func (e ErrorHandler) ErrQueryParser(ctx *fiber.Ctx, err error, message string) error {
 	if e.DevMode {
 		log.Printf("[err.QueryParser] error: %v\n", err)
 	}
 
-	return ctx.Status(fiber.StatusBadRequest).SendString(err.Error())
+	return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+		"error":   err,
+		"message": message,
+	})
 }
 
-func (e ErrorHandler) ErrRecordNotFound(ctx *fiber.Ctx, err error) error {
+func (e ErrorHandler) ErrRecordNotFound(ctx *fiber.Ctx, err error, message string) error {
 	if e.DevMode {
 		log.Printf("[err.RecordNotFound] error: %v\n", err)
 	}
 
-	return ctx.Status(fiber.StatusNotFound).SendString(err.Error())
+	return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{
+		"error":   err,
+		"message": message,
+	})
 }
 
-func (e ErrorHandler) ErrValidation(ctx *fiber.Ctx, err error) error {
+func (e ErrorHandler) ErrValidation(ctx *fiber.Ctx, err error, message string) error {
 	if e.DevMode {
 		log.Printf("[err.Validation] error: %v\n", err)
 	}
 
-	return ctx.Status(fiber.StatusBadRequest).SendString(err.Error())
+	return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+		"error":   err,
+		"message": message,
+	})
 }
 
-func (e ErrorHandler) ErrDatabase(ctx *fiber.Ctx, err error) error {
+func (e ErrorHandler) ErrDatabase(ctx *fiber.Ctx, err error, message string) error {
 	if e.DevMode {
 		log.Printf("[err.Database] error: %v\n", err)
 	}
 
-	return ctx.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+		"error":   err,
+		"message": message,
+	})
 }
 
-func (e ErrorHandler) ErrLogical(ctx *fiber.Ctx, err error) error {
+func (e ErrorHandler) ErrLogical(ctx *fiber.Ctx, err error, message string) error {
 	if e.DevMode {
 		log.Printf("[err.Logical] error: %v\n", err)
 	}
 
-	return ctx.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+		"error":   err,
+		"message": message,
+	})
 }
 
-func (e ErrorHandler) ErrUnauthorized(ctx *fiber.Ctx, err error) error {
+func (e ErrorHandler) ErrUnauthorized(ctx *fiber.Ctx, err error, message string) error {
 	if e.DevMode {
 		log.Printf("[err.Unauthorized] error: %v\n", err)
 	}
 
-	return ctx.Status(fiber.StatusUnauthorized).SendString(err.Error())
+	return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+		"error":   err,
+		"message": message,
+	})
 }
 
-func (e ErrorHandler) ErrAccess(ctx *fiber.Ctx, err error) error {
+func (e ErrorHandler) ErrAccess(ctx *fiber.Ctx, err error, message string) error {
 	if e.DevMode {
 		log.Printf("[err.Access] error: %v\n", err)
 	}
 
-	return ctx.Status(fiber.StatusForbidden).SendString(err.Error())
+	return ctx.Status(fiber.StatusForbidden).JSON(fiber.Map{
+		"error":   err,
+		"message": message,
+	})
 }
