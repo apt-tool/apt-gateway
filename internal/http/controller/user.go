9ae14228f2c794202c6809394669351a82d83d9b
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-06 20:29:59 +0330

    update: user error

--- internal/http/controller/user.go
+++ internal/http/controller/user.go
@@ -14,15 +14,27 @@ func (c Controller) CreateUser(ctx *fiber.Ctx) error {
 	req := new(request.UserRegisterRequest)
 
 	if err := ctx.BodyParser(req); err != nil {
-		return c.ErrHandler.ErrBodyParser(ctx, fmt.Errorf("[controller.user.Register] failed to parse body error=%w", err))
+		return c.ErrHandler.ErrBodyParser(
+			ctx,
+			fmt.Errorf("[controller.user.Register] failed to parse body error=%w", err),
+			MessageRequestBody,
+		)
 	}
 
 	if err := req.Validate(); err != nil {
-		return c.ErrHandler.ErrValidation(ctx, fmt.Errorf("[controller.user.Register] failed to validate request error=%w", err))
+		return c.ErrHandler.ErrValidation(
+			ctx,
+			fmt.Errorf("[controller.user.Register] failed to validate request error=%w", err),
+			MessageLoginErrValidation,
+		)
 	}
 
 	if err := c.Models.Users.Create(req.ToModel()); err != nil {
-		return c.ErrHandler.ErrDatabase(ctx, fmt.Errorf("[controller.user.Register] failed to create user error=%w", err))
+		return c.ErrHandler.ErrDatabase(
+			ctx,
+			fmt.Errorf("[controller.user.Register] failed to create user error=%w", err),
+			MessageFailedEntityCreate,
+		)
 	}
 
 	return ctx.SendStatus(fiber.StatusOK)
@@ -34,7 +46,11 @@ func (c Controller) GetUser(ctx *fiber.Ctx) error {
 
 	record, err := c.Models.Users.GetByID(uint(id))
 	if err != nil {
-		return c.ErrHandler.ErrRecordNotFound(ctx, fmt.Errorf("[controller.user.Get] username and password don't match error=%w", err))
+		return c.ErrHandler.ErrRecordNotFound(
+			ctx,
+			fmt.Errorf("[controller.user.Get] username and password don't match error=%w", err),
+			MessageFailedEntityList,
+		)
 	}
 
 	return ctx.Status(fiber.StatusOK).JSON(response.UserResponse{}.DTO(record))
@@ -44,7 +60,11 @@ func (c Controller) GetUser(ctx *fiber.Ctx) error {
 func (c Controller) GetUsersList(ctx *fiber.Ctx) error {
 	list, err := c.Models.Users.Get()
 	if err != nil {
-		return c.ErrHandler.ErrDatabase(ctx, fmt.Errorf("[controller.user.List] failed to get users error=%w", err))
+		return c.ErrHandler.ErrDatabase(
+			ctx,
+			fmt.Errorf("[controller.user.List] failed to get users error=%w", err),
+			MessageFailedEntityList,
+		)
 	}
 
 	records := make([]*response.UserResponse, 0)
@@ -63,23 +83,32 @@ func (c Controller) UpdateUser(ctx *fiber.Ctx) error {
 	req := new(request.UserRegisterRequest)
 
 	if err := ctx.BodyParser(&req); err != nil {
-		return c.ErrHandler.ErrBodyParser(ctx, fmt.Errorf("[controller.user.Update] failed to parse body error=%w", err))
+		return c.ErrHandler.ErrBodyParser(
+			ctx,
+			fmt.Errorf("[controller.user.Update] failed to parse body error=%w", err),
+			MessageRequestBody,
+		)
 	}
 
 	u, err := c.Models.Users.GetByID(uint(id))
 	if err != nil {
-		return c.ErrHandler.ErrRecordNotFound(ctx, fmt.Errorf("[controller.user.Update] failed to find user error=%w", err))
+		return c.ErrHandler.ErrRecordNotFound(
+			ctx,
+			fmt.Errorf("[controller.user.Update] failed to find user error=%w", err),
+			MessageFailedEntityUpdate,
+		)
 	}
 
 	u.Username = req.Name
 	u.Role = req.Role
-
-	if len(req.Pass) > 0 {
-		u.Password = req.Pass
-	}
+	u.Password = req.Pass
 
 	if er := c.Models.Users.Update(uint(id), u); er != nil {
-		return c.ErrHandler.ErrDatabase(ctx, fmt.Errorf("[controller.user.Update] failed to update user error=%w", err))
+		return c.ErrHandler.ErrDatabase(
+			ctx,
+			fmt.Errorf("[controller.user.Update] failed to update user error=%w", err),
+			MessageFailedEntityUpdate,
+		)
 	}
 
 	return ctx.SendStatus(fiber.StatusOK)
@@ -90,7 +119,11 @@ func (c Controller) DeleteUser(ctx *fiber.Ctx) error {
 	id, _ := ctx.ParamsInt("id")
 
 	if err := c.Models.Users.Delete(uint(id)); err != nil {
-		return c.ErrHandler.ErrDatabase(ctx, fmt.Errorf("[controller.user.Delete] failed to delete user error=%w", err))
+		return c.ErrHandler.ErrDatabase(
+			ctx,
+			fmt.Errorf("[controller.user.Delete] failed to delete user error=%w", err),
+			MessageFailedEntityRemove,
+		)
 	}
 
 	return ctx.SendStatus(fiber.StatusOK)
