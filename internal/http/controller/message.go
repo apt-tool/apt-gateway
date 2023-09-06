f1ab29023c007a50f68b28ac7215219ea67ab2ce
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-06 20:23:28 +0330

    update: namespace error

--- internal/http/controller/message.go
+++ internal/http/controller/message.go
@@ -2,6 +2,10 @@ package controller
 
 const (
 	MessageRequestBody           = "your request payload is could not be parsed, please try again!"
+	MessageFailedEntityRemove    = "something went wrong in delete process, try again!"
+	MessageFailedEntityList      = "sorry, we couldn't list your objects!"
+	MessageFailedEntityCreate    = "sorry, we couldn't create your object, try again!"
+	MessageFailedEntityUpdate    = "sorry, your object is not updated!"
 	MessageLoginErrValidation    = "your input data is not valid, please check your username and password again!"
 	MessageWrongCredentials      = "this username does not match with given password!"
 	MessageFailedTokenGeneration = "sorry, we couldn't create your token! please try to login again after few minutes"
--- internal/http/controller/namespace.go
+++ internal/http/controller/namespace.go
@@ -2,10 +2,11 @@ package controller
 
 import (
 	"fmt"
-	"github.com/apt-tool/apt-core/pkg/models/user"
 
 	"github.com/apt-tool/apt-gateway/internal/http/request"
 	"github.com/apt-tool/apt-gateway/internal/http/response"
+
+	"github.com/apt-tool/apt-core/pkg/models/user"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -17,11 +18,19 @@ func (c Controller) CreateNamespace(ctx *fiber.Ctx) error {
 	req := new(request.NamespaceRequest)
 
 	if err := ctx.BodyParser(&req); err != nil {
-		return c.ErrHandler.ErrBodyParser(ctx, fmt.Errorf("[controller.namespace.Create] failed to parse body error: %w", err))
+		return c.ErrHandler.ErrBodyParser(
+			ctx,
+			fmt.Errorf("[controller.namespace.Create] failed to parse body error: %w", err),
+			MessageRequestBody,
+		)
 	}
 
 	if err := c.Models.Namespaces.Create(req.ToModel(u.Username)); err != nil {
-		return c.ErrHandler.ErrDatabase(ctx, fmt.Errorf("[controller.namespace.Create] failed to create model error: %w", err))
+		return c.ErrHandler.ErrDatabase(
+			ctx,
+			fmt.Errorf("[controller.namespace.Create] failed to create model error: %w", err),
+			MessageFailedEntityCreate,
+		)
 	}
 
 	return ctx.SendStatus(fiber.StatusOK)
@@ -32,7 +41,11 @@ func (c Controller) DeleteNamespace(ctx *fiber.Ctx) error {
 	id, _ := ctx.ParamsInt("id", 0)
 
 	if err := c.Models.Namespaces.Delete(uint(id)); err != nil {
-		return c.ErrHandler.ErrDatabase(ctx, fmt.Errorf("[controller.namespace.Delete] failed to delete model error: %w", err))
+		return c.ErrHandler.ErrDatabase(
+			ctx,
+			fmt.Errorf("[controller.namespace.Delete] failed to delete model error: %w", err),
+			MessageFailedEntityRemove,
+		)
 	}
 
 	return ctx.SendStatus(fiber.StatusOK)
@@ -42,7 +55,11 @@ func (c Controller) DeleteNamespace(ctx *fiber.Ctx) error {
 func (c Controller) GetNamespacesList(ctx *fiber.Ctx) error {
 	list, err := c.Models.Namespaces.Get()
 	if err != nil {
-		return c.ErrHandler.ErrDatabase(ctx, fmt.Errorf("[controller.namespace.Get] failed to get models error: %w", err))
+		return c.ErrHandler.ErrDatabase(
+			ctx,
+			fmt.Errorf("[controller.namespace.GetList] failed to get models error: %w", err),
+			MessageFailedEntityList,
+		)
 	}
 
 	records := make([]*response.NamespaceResponse, 0)
@@ -61,15 +78,27 @@ func (c Controller) UpdateNamespace(ctx *fiber.Ctx) error {
 	req := new(request.NamespaceUpdateRequest)
 
 	if err := ctx.BodyParser(&req); err != nil {
-		return c.ErrHandler.ErrBodyParser(ctx, fmt.Errorf("[controller.namespace.Update] failed to parse body error= %w", err))
+		return c.ErrHandler.ErrBodyParser(
+			ctx,
+			fmt.Errorf("[controller.namespace.Update] failed to parse body error= %w", err),
+			MessageRequestBody,
+		)
 	}
 
 	if err := c.Models.UserNamespace.Clear(uint(id)); err != nil {
-		return c.ErrHandler.ErrDatabase(ctx, fmt.Errorf("[controller.namespace.Update] failed to remove records error=%w", err))
+		return c.ErrHandler.ErrDatabase(
+			ctx,
+			fmt.Errorf("[controller.namespace.Update] failed to remove records error=%w", err),
+			MessageFailedEntityUpdate,
+		)
 	}
 
 	if er := c.Models.UserNamespace.Create(uint(id), req.UserIDs); er != nil {
-		return c.ErrHandler.ErrDatabase(ctx, fmt.Errorf("[controller.namespace.Update] failed to update error= %w", er))
+		return c.ErrHandler.ErrDatabase(
+			ctx,
+			fmt.Errorf("[controller.namespace.Update] failed to update error= %w", er),
+			MessageFailedEntityUpdate,
+		)
 	}
 
 	// todo: update namespace
@@ -83,12 +112,20 @@ func (c Controller) GetUserNamespacesList(ctx *fiber.Ctx) error {
 
 	ids, err := c.Models.UserNamespace.GetNamespaces(u.ID)
 	if err != nil {
-		return c.ErrHandler.ErrDatabase(ctx, fmt.Errorf("[controller.namespace.User] failed to get ids error= %w", err))
+		return c.ErrHandler.ErrDatabase(
+			ctx,
+			fmt.Errorf("[controller.namespace.User] failed to get ids error= %w", err),
+			MessageFailedEntityList,
+		)
 	}
 
 	namespaces, err := c.Models.Namespaces.GetByIDs(ids)
 	if err != nil {
-		return c.ErrHandler.ErrDatabase(ctx, fmt.Errorf("[controller.namespace.User] failed to get namespaces error= %w", err))
+		return c.ErrHandler.ErrDatabase(
+			ctx,
+			fmt.Errorf("[controller.namespace.User] failed to get namespaces error= %w", err),
+			MessageFailedEntityList,
+		)
 	}
 
 	list := make([]*response.NamespaceResponse, 0)
@@ -104,7 +141,11 @@ func (c Controller) GetUserNamespacesList(ctx *fiber.Ctx) error {
 func (c Controller) GetUserNamespace(ctx *fiber.Ctx) error {
 	namespace, err := c.Models.Namespaces.GetByID(ctx.Locals("namespace").(uint))
 	if err != nil {
-		return c.ErrHandler.ErrRecordNotFound(ctx, fmt.Errorf("[controller.namespace.Get] failed to get projects error=%w", err))
+		return c.ErrHandler.ErrRecordNotFound(
+			ctx,
+			fmt.Errorf("[controller.namespace.GetUserNamespace] failed to get projects error=%w", err),
+			MessageFailedEntityList,
+		)
 	}
 
 	return ctx.Status(fiber.StatusOK).JSON(response.NamespaceResponse{}.DTO(namespace))
@@ -116,17 +157,29 @@ func (c Controller) GetNamespace(ctx *fiber.Ctx) error {
 
 	namespace, err := c.Models.Namespaces.GetByID(uint(id))
 	if err != nil {
-		return c.ErrHandler.ErrRecordNotFound(ctx, fmt.Errorf("[controller.namespace.Get] failed to get projects error=%w", err))
+		return c.ErrHandler.ErrRecordNotFound(
+			ctx,
+			fmt.Errorf("[controller.namespace.Get] failed to get projects error=%w", err),
+			MessageFailedEntityList,
+		)
 	}
 
 	ids, err := c.Models.UserNamespace.GetUsers(uint(id))
 	if err != nil {
-		return c.ErrHandler.ErrRecordNotFound(ctx, fmt.Errorf("[controller.namespace.Get] failed to get ids error=%w", err))
+		return c.ErrHandler.ErrRecordNotFound(
+			ctx,
+			fmt.Errorf("[controller.namespace.Get] failed to get ids error=%w", err),
+			MessageFailedEntityList,
+		)
 	}
 
 	users, err := c.Models.Users.GetByIDs(ids)
 	if err != nil {
-		return c.ErrHandler.ErrRecordNotFound(ctx, fmt.Errorf("[controller.namespace.Get] failed to get users error=%w", err))
+		return c.ErrHandler.ErrRecordNotFound(
+			ctx,
+			fmt.Errorf("[controller.namespace.Get] failed to get users error=%w", err),
+			MessageFailedEntityList,
+		)
 	}
 
 	namespace.Users = users
