355889460aed4f1855d2d68ce1e4ccb8520654a0
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-03 12:27:45 +0330

    update: middlewares

--- internal/http/middleware/auth.go
+++ internal/http/middleware/auth.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,14 +9,7 @@ import (
 // Auth middleware check user authentication.
 func (m Middleware) Auth(ctx *fiber.Ctx) error {
 	if token := ctx.Get("x-token", ""); token != "" {
-		if name, err := m.JWTAuthenticator.ParseToken(token); err == nil {
-			user, er := m.Models.Users.GetByName(name)
-			if er != nil {
-				return m.ErrHandler.ErrRecordNotFound(ctx, fmt.Errorf("user not found"), "user not found")
-			}
-
-			ctx.Locals("user", user)
-
+		if _, err := m.JWTAuthenticator.ParseToken(token); err == nil {
 			return ctx.Next()
 		} else {
 			return m.ErrHandler.ErrUnauthorized(ctx, err, "you are not logged in!")
--- internal/http/middleware/middleware.go
+++ internal/http/middleware/middleware.go
@@ -3,12 +3,9 @@ package middleware
 import (
 	"github.com/ptaas-tool/gateway/internal/http/controller/handler"
 	"github.com/ptaas-tool/gateway/internal/utils/jwt"
-
-	"github.com/ptaas-tool/base-api/pkg/models"
 )
 
 type Middleware struct {
 	JWTAuthenticator jwt.Authenticator
-	Models           *models.Interface
 	ErrHandler       handler.ErrorHandler
 }
