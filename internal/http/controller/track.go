649014292e1e0fb0802bc37eddc04777159bde8e
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-26 19:18:26 +0330

    add: track handler

--- internal/http/controller/track.go
+++ internal/http/controller/track.go
@@ -1,1 +1,32 @@
 package controller
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/ptaas-tool/gateway/internal/http/response"
+)
+
+func (c Controller) GetTracksList(ctx *fiber.Ctx) error {
+	tmp, _ := ctx.ParamsInt("project_id", 0)
+	projectID := uint(tmp)
+
+	id := uint(ctx.QueryInt("id", 0))
+
+	tracks, err := c.Models.Tracks.Get(id, projectID)
+	if err != nil {
+		return c.ErrHandler.ErrRecordNotFound(
+			ctx,
+			fmt.Errorf("[controller.Tracks.Get] record not found error=%w", err),
+			MessageFailedEntityList,
+		)
+	}
+
+	records := make([]*response.TrackResponse, 0)
+
+	for _, track := range tracks {
+		records = append(records, response.TrackResponse{}.DTO(track))
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(records)
+}
